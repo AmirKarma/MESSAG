d57faa9e9d51a6a9969c64ff87f9b7c3a80d7ac5
Louis Bieber <louis-bieber@gmx.de>
2023-11-14 11:49:24 +0100

    thomasSceneChaange
    thomasSceneChaange

--- View/Welt/world.gd
+++ View/Welt/world.gd
@@ -4,3 +4,10 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	y_sort_enabled = true
+	
+func _process(_delta):
+		if Input.is_action_just_pressed("debug"):
+			var minigame = load("res://Minigame1/minigame_1.tscn").instantiate()
+			get_tree().root.add_child(minigame)
+			get_tree().current_scene.queue_free()
+			get_tree().current_scene = minigame

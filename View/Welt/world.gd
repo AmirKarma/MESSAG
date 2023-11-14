91f5a5be5a9bc163226e5184ebd3ac6ff3e4f022
Thomas Kesper <thomas.kesper@mni.thm.de>
2023-11-14 11:03:56 +0100

    MinigameSzene_add

--- View/Welt/world.gd
+++ View/Welt/world.gd
@@ -1,6 +1,14 @@
 extends Node2D
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	y_sort_enabled = true
+	self.y_sort_enabled = true
+
+# Process function is called every frame.
+# The parameter "delta" represents the time passed since the last frame.
+func _process(_delta):
+	if Input.is_action_just_pressed("minigame1"):
+		var minigame_scene = preload("res://Welt/minigame_1.tscn").instantiate()
+		get_tree().root.add_child(minigame_scene)
+		get_tree().current_scene.queue_free()
+		get_tree().current_scene = minigame_scene

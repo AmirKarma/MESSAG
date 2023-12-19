1e7b1b0bb5c3e3f251b48e811eb6eb6d69b61341
AmirKarma <amir.ebrahimi0k@gmail.com>
2023-12-19 21:00:44 +0100

    add Building Image

--- View/Optionbar/optionbarRect.gd
+++ View/Optionbar/optionbarRect.gd
@@ -1,16 +1,22 @@
 extends NinePatchRect
- 
+
+@onready var buildingRect = $buildingRect 
 var optionbarRectSize = self.size
+var buildingImagePos
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	buildingImagePos = buildingRect.size/2
+	buildingRect.get_child(0).position = buildingImagePos
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-
+func set_building_image(path : String):
+	buildingRect.get_child(0).texture = load(path)
+	
 func _on_texture_button_pressed():
 	self.hide()	

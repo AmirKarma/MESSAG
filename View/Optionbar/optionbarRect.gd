514368f9adf31cf263a6b7146af531a30744279e
AmirKarma <amir.ebrahimi0k@gmail.com>
2023-12-19 20:28:37 +0100

    add close button

--- View/Optionbar/optionbarRect.gd
+++ View/Optionbar/optionbarRect.gd
@@ -1,13 +1,16 @@
 extends NinePatchRect
-
-var optionbarRectSize = Vector2(320,180)
+ 
+var optionbarRectSize = self.size
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	self.size = optionbarRectSize
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_texture_button_pressed():
+	self.hide()	

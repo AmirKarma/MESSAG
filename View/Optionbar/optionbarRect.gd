70136c0f3e28675505bd24fb0fc5fc06098883f5
AmirKarma <amir.ebrahimi0k@gmail.com>
2023-12-19 20:21:58 +0100

    added Overview

---
+++ View/Optionbar/optionbarRect.gd
@@ -0,0 +1,13 @@
+extends NinePatchRect
+
+var optionbarRectSize = Vector2(320,180)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	self.size = optionbarRectSize
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass

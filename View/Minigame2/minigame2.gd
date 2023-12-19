ad96964cfcd22d65b1dd9c1166d7d207b98974bd
Finn <76914941+finn762@users.noreply.github.com>
2023-12-19 15:01:05 +0100

    Charaktersteuerung (#82)

--- View/Minigame2/minigame2.gd
+++ View/Minigame2/minigame2.gd
@@ -6,22 +6,22 @@ var xValues = [64,128,192,256]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	DataScript.minigame2_timer = 1
+	DataScript.minigame2_timer = 3
+
 
 func _on_timer_timeout():
-	print('timeout')
 	match str(DataScript.minigame2_timer).pad_decimals(0):
-		'2':
+		'4':
 			DataScript.minigame2_timerSpeed = 0.005 
 			#cometSpeedValues[0] = Vector2(0,60)
-		'3': 
+		'5': 
 			DataScript.minigame2_timerSpeed  = 0.0025 
 			#cometSpeedValues[1] = Vector2(0,70)
 			#commetSpawnCount = 2
-		'4': 
+		'6': 
 			DataScript.minigame2_timerSpeed  = 0.00125 
 			#cometSpeedValues[2] = Vector2(0,80)
-		#'5':
+		#'7':
 			#cometSpeedValues[3] = Vector2(0,90)
 			#commetSpawnCount = 3
 		

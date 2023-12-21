ef23a01df47932a264ead847cf7f4b7a980bda38
Amir Ebrahimi <99660768+AmirKarma@users.noreply.github.com>
2023-12-21 18:03:52 +0100

    Optionbar - SWTPIIWS2022G3-146 (#86)
    * added Overview
    
    * add close button
    
    * add Building Image
    
    * optionbar in world screen
    
    * improved optionbar in world scene
    
    * add buttons to optionbar
    
    * add name and level to optionbar
    
    * fix label position
    
    * add ressourcebar to opzionbar

--- View/Welt/Rocket/rocket.gd
+++ View/Welt/Rocket/rocket.gd
@@ -10,7 +10,6 @@ var player
 func _ready():
 	world = get_tree().get_root().get_node("World")
 	player = world.get_node("Player")
-	add_items()
 		
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,7 +17,6 @@ func _process(delta):
 	if pressed:
 		if player.position.distance_to(position) < 50:
 			pressed = false
-			doSomething()
 		elif player.nav.target_position != position - Vector2(20,20):
 			pressed = false
 
@@ -32,26 +30,3 @@ func _on_option_button_pressed():
 		player.nav.target_position = position - Vector2(20,20)
 		player.animationState.travel("Run")
 
-
-func doSomething():
-	optionButton.show_popup()
-	optionButton.get_popup().position = Vector2(100, 125)
-
-func add_items():
-	optionButton.add_item("Minigame")
-	optionButton.add_item("Minigame2")
-
-func _on_option_button_item_selected(index):
-	
-	if index == 0:
-		DataScript.setUnixLastTime(Time.get_unix_time_from_system())
-		var minigame = load("res://Minigame1/minigame_1.tscn").instantiate()
-		get_tree().root.add_child(minigame)
-		get_tree().current_scene.queue_free()
-		get_tree().current_scene = minigame
-	if index == 1:
-		DataScript.setUnixLastTime(Time.get_unix_time_from_system())
-		var minigame2 = load("res://Minigame2/minigame2.tscn").instantiate()
-		get_tree().root.add_child(minigame2)
-		get_tree().current_scene.queue_free()
-		get_tree().current_scene = minigame2	
--- View/Welt/world.gd
+++ View/Welt/world.gd
@@ -3,18 +3,25 @@
 
 extends Node2D
 
+@onready var optionbar = $optionbar
+@onready var player = $Player
+var optionbar_pos
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	y_sort_enabled = true
 	get_tree().set_auto_accept_quit(false)
+	print()
 	
 # Process function called every frame
 func _process(_delta):
+		
 		if Input.is_action_just_pressed("debug"):
 			var minigame = load("res://Minigame1/minigame_1.tscn").instantiate()
 			get_tree().root.add_child(minigame)
 			get_tree().current_scene.queue_free()
 			get_tree().current_scene = minigame
+		building_distance(self.get_child(2)) 
 
 # Notification function called for window management events
 func _notification(what):
@@ -26,3 +33,40 @@ func _notification(what):
 	elif what == NOTIFICATION_WM_GO_BACK_REQUEST:
 		DataScript.setUnixLastTime(Time.get_unix_time_from_system())
 		get_tree().quit()
+
+func building_distance(building : Node):
+	if(building.pressed == true):
+		optionbar.set_visible(false)
+		if player.position.distance_to(building.position) > 50:
+			player.stand_still = false
+			player.moving = true
+			player.nav.target_position = building.position - Vector2(20,20)
+			player.animationState.travel("Run")
+		open_optionbar(building)
+	elif optionbar.is_close_button_pressed():
+		hide_optionbar()
+		
+		
+			
+func open_optionbar(building : Node):
+	if building.pressed:
+		if player.position.distance_to(building.position) < 50:
+			building.pressed = false
+			optionbar_pos = player.get_node("Camera2D").get_screen_center_position() - get_viewport_rect().size / 2
+			optionbar.set_optionbar(optionbar_pos,"Rocket","1",building.get_node("rocketSprite").texture.resource_path , "res://Minigame1/minigame_1.tscn")
+			optionbar.set_visible(true)
+			optionbar.close_pressed = false
+			player.stand_still = true
+			player.set_process(false)
+			player.set_physics_process(false)
+			player.get_node("Camera2D/HUD").visible = false
+		elif player.nav.target_position != building.position - Vector2(20,20):			
+			building.pressed = false
+
+func hide_optionbar():
+	optionbar.set_visible(false)
+	player.set_process(true)
+	player.set_physics_process(true)
+	player.get_node("Camera2D/HUD").visible = true
+
+		

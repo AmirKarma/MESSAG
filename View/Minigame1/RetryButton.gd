98909bbefdaec9638c37e83f9b691d25ab91c6e4
Thomas <148204697+tkspr@users.noreply.github.com>
2024-01-14 00:53:56 +0100

    Commets and fixes (#121)

--- View/Minigame1/RetryButton.gd
+++ View/Minigame1/RetryButton.gd
@@ -1,4 +1,6 @@
 extends Button
 
+# Function called when the button is pressed
 func _pressed():
+	# Change the scene to the specified file
 	get_tree().change_scene_to_file("res://Minigame1/minigame_1.tscn")
